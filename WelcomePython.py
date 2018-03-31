0b6229468c2e44f772fa0023b33c221efd7b8f0d
R00078187 <robert.polak@mycit.ie>
2018-03-31 18:53:17 +0100

    Commit Version 4

--- WelcomePython.py
+++ WelcomePython.py
@@ -2,6 +2,7 @@ import getpass
 import datetime
 import os
 print ("Welcome",getpass.getuser()+",")
+print ("Another Beautiful Day !!!")
 now = datetime.datetime.now()
 print ("Current date and time : ", now.strftime("%Y-%m-%d %H:%M:%S"))
 cwd = os.getcwd()
